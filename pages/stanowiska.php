ed9034b427ce82743d7fbc9ad29aacf9f734d75b
cgt19kaszad <dawid1kasza1@interia.pl>
2022-11-24 11:02:04 +0100

    yujhidfvsfreth

--- pages/stanowiska.php
+++ pages/stanowiska.php
@@ -7,9 +7,9 @@ $result = mysqli_query($conn,$query);
 if(mysqli_num_rows($result) > 0) {
     echo '<p>Zawiera ' . mysqli_num_rows($result) . ' wierszy</p>';
     echo '<table>';
-    echo '<tr><th>Nr_stanowisko</th><th>Nazwa</th></tr>';
+    echo '<tr><th>Id stanowiska</th><th>Nazwa</th></tr>';
     while ($row = mysqli_fetch_assoc($result)) {
-        echo '<tr><td>' . $row['Id_stanowisko'] . '</td><td>' . $row['Nazwa'] . '</td></tr>';
+        echo '<tr><td>' . $row['Id_stanowisko'] . '</td><td>' . $row['Nazwa'] . '</td><td>'. '<a href="?page=stanowiska_formularz&stanowiska=' . $row['Id_stanowisko'] .' ">Edycja</a>' . '</td></tr>';
     }
     echo '</table>';
 }
